43d6feeed9eb2c1c243d7c58e06092cf1161c24f
Tomasz Tracz <t@tracz.me>
2022-07-14 12:42:39 +0100

    Add more tests. add more checks and abs values

--- src/iterate.test.ts
+++ src/iterate.test.ts
@@ -1,9 +1,50 @@
 import iterate from './iterate';
 import counter from './testingHelpers/counter';
 
-const count = counter();
+test('runs with a 0, x 0, returns 0', () => {
+  const count = counter();
+  iterate(0, 0, count);
+  expect(count(true)).toBe(0);
+});
+
+test('runs with a 1, x -1, returns 0', () => {
+  const count = counter();
+  iterate(1, -1, count);
+  expect(count(true)).toBe(0);
+});
+
+test('runs with a -1, x 1, returns 0', () => {
+  const count = counter();
+  iterate(-1, 1, count);
+  expect(count(true)).toBe(0);
+});
+
+test('runs with a 1, x -100, returns 0', () => {
+  const count = counter();
+  iterate(1, -100, count);
+  expect(count(true)).toBe(0);
+});
 
 test('runs with a 1, x 1, returns 3', () => {
+  const count = counter();
   iterate(1, 1, count);
+  expect(count(true)).toBe(3);
+});
+
+test('runs with a 10, x 100, returns 53', () => {
+  const count = counter();
+  iterate(10, 100, count);
+  expect(count(true)).toBe(53);
+});
+
+test('runs with a 9, x 13, returns 6', () => {
+  const count = counter();
+  iterate(9, 13, count);
+  expect(count(true)).toBe(6);
+});
+
+test('runs with a -1, x -1, returns 3', () => {
+  const count = counter();
+  iterate(-1, -1, count);
   expect(count(true)).toBe(3);
 });
--- src/iterate.ts
+++ src/iterate.ts
@@ -3,18 +3,19 @@ export default function iterate(
   x: number,
   code: () => any = () => {},
 ) {
-  iterator(a, x, code);
-  iterator(a + 1, x * 2, code);
-  iterator(a + 2, x * 3, code);
+  if (a === 0 || x === 0) return;
+  if((a / x) < 0) return; // check if the numbers had opposite sign
+  
+  const a_abs = Math.abs(a);
+  const x_abs = Math.abs(x)
+
+  iterator(a_abs, x_abs, code);
+  iterator(a_abs + 1, x_abs * 2, code);
+  iterator(a_abs + 2, x_abs * 3, code);
 }
 
 function iterator(a: number, x: number, code: () => any) {
-  if (a === 0 || x === 0) return;
-
   const multiples = Math.floor(x / a);
-
-  if (multiples <= 0) return; // check if the numbers had opposite sign
-
   for (let i = 0; i < multiples; i++) {
     code();
   }
